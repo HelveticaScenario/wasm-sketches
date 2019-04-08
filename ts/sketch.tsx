7b638cc049a4df51239ab621b2b3521eb2a6fdc0
Dan Lewis <daniel.lewis.software.engineer@gmail.com>
2019-04-08 10:33:44 -0500

    sketch component

--- ts/sketch.tsx
+++ ts/sketch.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { RouteComponentProps } from 'react-router-dom'
-import { WasmContext, SketchDescription } from './wasmContext'
+import {
+	WasmContext,
+	SketchDescription,
+	unwrapContextValue,
+} from './wasmContext'
+import { uw } from './utils'
+import runSketch from './runSketch'
 
 export interface SketchProps extends RouteComponentProps {
 	sketch: SketchDescription
@@ -9,13 +15,40 @@ export interface SketchProps extends RouteComponentProps {
 export default class Sketch extends React.Component<SketchProps, {}> {
 	static contextType = WasmContext
 	context!: React.ContextType<typeof WasmContext>
-	canvasRef: React.Ref<HTMLCanvasElement>
+	canvasRef: React.RefObject<HTMLCanvasElement>
+	run?: (now: number) => void
 	constructor(props: SketchProps) {
 		super(props)
 		this.canvasRef = React.createRef()
 	}
 
-	componentDidMount() {}
+	loadSketch = (props: SketchProps) => {
+		const { sketch } = props
+		const { module } = unwrapContextValue(this.context)
+		const canvas = uw(this.canvasRef.current)
+		const run = runSketch(canvas, sketch, module)
+		this.run = run
+
+		const raf = (now: number) => {
+			if (this.run === run) {
+				requestAnimationFrame(raf)
+				run(now)
+			}
+		}
+		raf(performance.now())
+	}
+
+	componentDidMount() {
+		this.loadSketch(this.props)
+	}
+	componentDidUpdate(prevProps: SketchProps) {
+		if (prevProps.sketch !== this.props.sketch) {
+			this.loadSketch(this.props)
+		}
+	}
+	componentWillUnmount() {
+		this.run = undefined
+	}
 
 	render() {
 		return <canvas ref={this.canvasRef} />
